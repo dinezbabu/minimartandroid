9f9666507b0510d1ac216ce835b36cbcc0fdcafd
BABU, DINESH <dbabu@mast.com>
2019-08-12 21:49:17 +0530

    Home Page Design

--- app/src/main/java/com/minimart/groceries/MainActivity.java
+++ app/src/main/java/com/minimart/groceries/MainActivity.java
@@ -2,8 +2,15 @@ package com.minimart.groceries;
 
 import android.app.Activity;
 import android.content.Intent;
+
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,13 +19,29 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
     private Button scanBtn;
     private TextView formatTxt, contentTxt;
+    private SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.activity_main);
+
+        searchView=(SearchView) findViewById(R.id.txtSearch);
+        searchView.setIconifiedByDefault(false);
+       searchView.setQueryHint("Search minimart products");
+
+        final ActionBar actionBar= getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
         scanBtn = (Button)findViewById(R.id.scan_button);
         formatTxt = (TextView)findViewById(R.id.scan_format);
         contentTxt = (TextView)findViewById(R.id.scan_content);
@@ -30,8 +53,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
         if (scanningResult != null) {
             String scanContent = scanningResult.getContents();
             String scanFormat = scanningResult.getFormatName();
-            formatTxt.setText("FORMAT: " + scanFormat);
-            contentTxt.setText("CONTENT: " + scanContent);
+            //formatTxt.setText("FORMAT: " + scanFormat);
+            //contentTxt.setText("CONTENT: " + scanContent);
+            searchView.setQuery(scanContent,false);
         }
         else{
             Toast toast = Toast.makeText(getApplicationContext(),
@@ -46,5 +70,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
             IntentIntegrator scanIntegrator = new IntentIntegrator(this);
             scanIntegrator.initiateScan();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
